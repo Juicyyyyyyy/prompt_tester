10ae6a7707c10ff635c37fd5291971a8e3e5b466
Juicyyyyyyy <declan.lowee@gmail.com>
2023-10-25 01:06:20 +0200

    First version

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import openai
+from multiprocessing import Pool
+import os
+from dotenv import load_dotenv
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Load environment variables from .env
+load_dotenv()
 
+# Define the function to process a single prompt
+def process_prompt(_):
+    # Initialize OpenAI API with the key from the environment
+    api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = api_key
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    with open('prompt.txt', 'r') as prompt_file:
+        prompt = prompt_file.read()  # Read the entire prompt as a single string
 
+    # Process the prompt and return the result
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",  # Choose the appropriate chat model
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ]
+    )
+    return response.choices[0].message['content']
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    # Define the number of times you want to test the same prompt
+    num_tests = 10  # You can change this to the desired number of tests
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Create a list of dummy values to use as placeholders for concurrent processing
+    prompt_list = [None] * num_tests
+
+    # Create a pool of processes to process prompts concurrently
+    with Pool(processes=4) as pool:  # Adjust the number of processes as needed
+        results = pool.map(process_prompt, prompt_list)
+
+    # Write the results to output.txt
+    with open('output.txt', 'w') as output_file:
+        for result in results:
+            output_file.write(result + '\n')
+
+    print("All prompts processed, and results written to output.txt.")
